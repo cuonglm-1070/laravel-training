044d1adaf4404b955ab98de6577a6e0cebd3a2a0
le.manh.cuong <SUN-ASTERISK\le.manh.cuong@i121070-pc.sun-asterisk.com>
2020-12-07 17:24:54 +0700

    add tables and relations

---
+++ database/migrations/2020_12_07_093051_create_passports_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePassportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('passports', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('nationality');
+            $table->timestamp('expired_at');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('passports', function (Blueprint $table) {
+            $table->dropForeign('passports_user_id_foreign');
+        });
+        Schema::dropIfExists('passports');
+    }
+}
